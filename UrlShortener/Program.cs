237fc19b2f6937e8a7f473bd326d67ea2213cbf2
Jeevanabhi <jeevanjenin@gmail.com>
2026-08-03 22:49:49 +0530

    Phase 2: Dockerize API and MySQL with EF Core migrations

--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -4,19 +4,24 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<UrlService>();
-var connectionString = builder.Configuration.GetConnectionString("Connectstring");
+// var connectionString = builder.Configuration.GetConnectionString("Connectstring");
+// builder.Services.AddDbContext<UrlShorten>(options =>
+// {
+//     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+// });
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<UrlShorten>(options =>
-{
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-});
+    options.UseMySql(
+        connectionString,
+        new MySqlServerVersion(new Version(8, 0, 36))));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.MapControllers();
 app.Run();
--- UrlShortener/obj/Debug/net9.0/UrlShortener.AssemblyInfo.cs
+++ UrlShortener/obj/Debug/net9.0/UrlShortener.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c68c4ac6108e9a1ceddc520dcde0400051917321")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81b42badce6c5242cbdac4b4bba7dd165afc6b21")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
